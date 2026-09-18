8221440b6289b05f1f4e4c006e7b847e8e7a1646
prathameshpatil5559-oss <prathameshpatil5559@gmail.com>
2026-09-18 10:03:13 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,26 @@
+import 'package:flutter/material.dart';
+import 'screens/dashboard_screen.dart';
 
+void main() {
+  runApp(const InstagramRaftarApp());
+}
+
+class InstagramRaftarApp extends StatelessWidget {
+  const InstagramRaftarApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Instagram Raftar',
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.deepPurple,
+        ),
+        scaffoldBackgroundColor: Colors.white,
+      ),
+      home: const DashboardScreen(),
+    );
+  }
+}
